b161d35dd072a833f7863f86479abe7022976581
John Cheng <jc2011@scarletmail.rutgers.edu>
2019-10-17 17:52:01 -0400

    made it better

--- Data_Scripts/getgamedata.py
+++ Data_Scripts/getgamedata.py
@@ -1,7 +1,7 @@
 import mysql.connector
 from nba_api.stats.endpoints import leaguegamelog
 import database
-game = leaguegamelog.LeagueGameLog(season=2018-20)
+game = leaguegamelog.LeagueGameLog(season='2018-19')
 game = game.get_dict()
 mydb = mysql.connector.connect(
     host =  database.databaseInfo["host"],
@@ -12,7 +12,7 @@ mydb = mysql.connector.connect(
 mycursor = mydb.cursor()
 
 #create table and add game data for each game
-mycursor.execute("CREATE TABLE IF NOT EXISTS games (season INT, teamid INT, teamname VARCHAR(20),gamedate DATE, matchup VARCHAR (255), gameid INT)")
+mycursor.execute("CREATE TABLE IF NOT EXISTS games (season INT, teamid INT, teamname VARCHAR(255),gamedate DATE, matchup VARCHAR (255), gameid INT)")
 for games in game["resultSets"][0]["rowSet"]:
     sql = "INSERT INTO games(season,teamid,teamname,gamedate,matchup,gameid) VALUES (%s,%s,%s,%s,%s,%s)"
     val = (games[0],games[1],games[3],games[5],games[6],games[4])
